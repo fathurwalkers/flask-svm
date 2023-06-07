5b8cdcd1f4e1d22a49a45142bd16bcbeb72da018
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-07 13:16:01 -0700

    update

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_url_path='',
+            static_folder='public',)
 
-@app.route("/cek")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.debug = True
+
+@app.route("/")
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5002)
