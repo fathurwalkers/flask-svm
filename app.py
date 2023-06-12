ed86195559717138bf754747386ccdb6ea748238
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-12 12:05:30 -0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+import pymysql
+import pandas as pd
+import snscrape.modules.twitter as sntwitter 
 
 app = Flask(__name__, 
             static_url_path='',
@@ -6,21 +9,41 @@ app = Flask(__name__,
 app.secret_key = "sangatrahasia"
 app.debug = True
 
+def connect():    
+    return pymysql.connect(host="localhost", user="root", password="", database="aplikasi_svm", charset='utf8mb4')
+
 @app.route("/")
 def home():
     return redirect(url_for('dashboard'))
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    con = connect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM login")
+    login = cursor.fetchall()
+    return render_template('dashboard.html', login=login)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/dashboard/crawling')
+@app.route('/dashboard/crawling', methods=('GET', 'POST'))
 def dashboard_crawling():
-    return render_template('crawling.html')
+    data_tweets = []
+    if request.method == 'POST':
+        kata_kunci = request.form['kata_kunci']
+        jumlah_tweet = request.form['jumlah_tweet']
+        maxTweets = int(jumlah_tweet)
+        print("start scraping")
+        cari_kata = kata_kunci + " since:2022-09-01 until:2023-01-01 lang:id"
+        print(cari_kata)
+        for tweet in sntwitter.TwitterSearchScraper(cari_kata).get_items():
+            if len(data_tweets) == maxTweets :
+                break
+            else:
+                data_tweets.append([tweet.date, tweet.user.username, tweet.content])
+    return render_template('crawling.html', data_tweets=data_tweets)
 
 @app.route('/dashboard/pre-processing')
 def dashboard_preprocessing():
--- db_config.py
+++ db_config.py
@@ -1,11 +1,8 @@
-from app import app
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'aplikasi_svm'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'aplikasi_svm'
+ 
+mysql = MySQL(app)
